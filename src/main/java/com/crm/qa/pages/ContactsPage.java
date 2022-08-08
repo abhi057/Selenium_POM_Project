9ff234d6982541ea7b0e6271e508cb6c0377c45c
abhi057 <abhi_057@yahoo.com>
2022-08-08 15:23:36 +0530

    Added new script for Create Contacts and added test data driven capability

--- src/main/java/com/crm/qa/pages/ContactsPage.java
+++ src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -17,6 +17,21 @@ public class ContactsPage extends TestBase {
 	@FindBy(xpath = "//div[class='ui checked fitted read-only checkbox'] label")
 	WebElement Checkbox1;
 
+	@FindBy(xpath = "//*[@href='/contacts/new']")
+	WebElement createNewContactBtn;
+
+	@FindBy(xpath = "//input[@name='first_name']")
+	WebElement firstname;
+
+	@FindBy(xpath = "//input[@name='last_name']")
+	WebElement lastname;
+
+	@FindBy(xpath = "//div[@name='company']//input[@type='text']")
+	WebElement company;
+	
+	@FindBy(xpath = "//*[@class='save icon']//parent::button")
+	WebElement saveBtn;
+
 	// initiate page elements
 	public ContactsPage() {
 		PageFactory.initElements(driver, this);
@@ -27,8 +42,16 @@ public class ContactsPage extends TestBase {
 
 	}
 
+	public void addContact(String fn, String ln, String comp) {
+		createNewContactBtn.click();
+		firstname.sendKeys(fn);
+		lastname.sendKeys(ln);
+		company.sendKeys(comp);
+		saveBtn.click();
+	}
+
 	public boolean selectContacts(String name) {
-		
+
 //method-1		
 
 //		String beforexpath = "//*[@class='table-wrapper']/table/tbody/tr[";
@@ -45,18 +68,16 @@ public class ContactsPage extends TestBase {
 //			}
 
 //		}
-		
-		
-		//Method-2
-		WebElement checkbox = driver.findElement(By.xpath("//*[contains(text(), '"+name+"')]/parent::td//preceding-sibling::td//div/input")); 
-		
-		JavascriptExecutor js = (JavascriptExecutor)driver;
+
+		// Method-2
+		WebElement checkbox = driver.findElement(
+				By.xpath("//*[contains(text(), '" + name + "')]/parent::td//preceding-sibling::td//div/input"));
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click()", checkbox);
-		
+
 		return checkbox.isSelected();
-	
-		
-		
+
 	}
 
 }
--- src/test/java/com/crm/qa/test/ContactsPageTest.java
+++ src/test/java/com/crm/qa/test/ContactsPageTest.java
@@ -3,18 +3,21 @@ package com.crm.qa.test;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.ContactsPage;
 import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
 
 public class ContactsPageTest extends TestBase{
 	
 	LoginPage loginPage;
 	HomePage homePage;  
 	ContactsPage contactsPage;
+	String sheetName = "Contacts";
 	
 	public ContactsPageTest(){
 		super(); 
@@ -32,15 +35,30 @@ public class ContactsPageTest extends TestBase{
 	}  
 	
 	
-    @Test(priority=1)
-    public void verifyContactsPagelabel() {
-    	Assert.assertTrue(contactsPage.verifyContactsPageLabel());
+//    @Test(priority=1)
+//    public void verifyContactsPagelabel() {
+//    	Assert.assertTrue(contactsPage.verifyContactsPageLabel());
+//    }
+    
+//    @Test(priority=2)
+//    public void selectContactsCheckbox() {
+//    	Assert.assertTrue(contactsPage.selectContacts("Abhi a")); 
+//    	Assert.assertTrue(contactsPage.selectContacts("Vid M"));    
+//          
+//    }
+    
+    @DataProvider
+    public Object[][] getCRMTestData() {
+    	Object data[][] = TestUtil.getTestData(sheetName);
+    	return data;
     }
     
-    @Test(priority=2)
-    public void selectContactsCheckbox() {
-    	Assert.assertTrue(contactsPage.selectContacts("Vid M"));    
-          
+    
+    @Test(priority=3, dataProvider = "getCRMTestData")
+    public void enterContacts(String fn, String ln, String comp) {
+    	
+    	//contactsPage.addContact("Rahul", "Dravid", "BCCI");
+    	contactsPage.addContact(fn, ln, comp);
     }
     
 	
